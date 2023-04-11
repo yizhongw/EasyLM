02b2bdad8a8810fbb1ebd2efa1452b4e0162e0fa
Hamish Ivison <hamishivi@gmail.com>
2023-04-11 13:30:48 -0700

    fix batch size

--- EasyLM/data.py
+++ EasyLM/data.py
@@ -53,14 +53,13 @@ class DatasetFactory(object):
         elif config.type == 'json':
             return JsonDataset(config.json_dataset, tokenizer, text_processor, **kwargs)
         elif config.type == 'json_torch':
-            torch.manual_seed(0)
             dataset = JsonTorchDataset(config.json_torch_dataset, tokenizer, text_processor, **kwargs)
             sampler = torch.utils.data.distributed.DistributedSampler(
                 dataset, num_replicas=jax.process_count(), rank=jax.process_index(), drop_last=True
             )
             return DataLoader(
                 dataset,
-                batch_size=config.json_torch_dataset.batch_size,
+                batch_size=config.json_torch_dataset.batch_size * jax.process_count(),
                 num_workers=config.json_torch_dataset.num_workers,
                 sampler=sampler,
                 collate_fn=numpy_collate,
