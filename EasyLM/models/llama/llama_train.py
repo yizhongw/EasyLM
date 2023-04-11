be625a5d0b44efe07ad21a2acd3ede2932c0b3a2
Hamish Ivison <hamishivi@gmail.com>
2023-04-11 09:16:47 -0700

    remove debug

--- EasyLM/models/llama/llama_train.py
+++ EasyLM/models/llama/llama_train.py
@@ -281,10 +281,6 @@ def main(argv):
                         'loss_masks': batch[1],
                         'attention_masks': batch[2],
                     }
-                    print(batch['tokens'][0])
-                    print('----------')
-                    print(batch['tokens'][1])
-                    print('----------')
                 train_state, sharded_rng, metrics = sharded_train_step(
                     train_state, sharded_rng, batch
                 )
