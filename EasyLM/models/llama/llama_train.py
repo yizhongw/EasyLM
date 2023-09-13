0313026e630a231acd6e6a720445311c46ad11d8
Hamish Ivison <hamishivi@gmail.com>
2023-09-12 22:03:09 -0700

    num epochs

--- EasyLM/models/llama/llama_train.py
+++ EasyLM/models/llama/llama_train.py
@@ -42,6 +42,7 @@ FLAGS, FLAGS_DEF = mlxu.define_flags_with_default(
     save_model_freq=0,
     save_milestone_freq=0,
     eval_steps=0,
+    num_epochs=0,
     tokenizer=LLaMAConfig.get_tokenizer_config(),
     train_dataset=DatasetFactory.get_default_config(),
     eval_dataset=DatasetFactory.get_default_config(),
