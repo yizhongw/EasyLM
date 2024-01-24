cf8a186b55c9ca591645c52d4406fb08256984d7
yizhongw <eastonwyz@gmail.com>
2024-01-24 03:23:13 -0800

    Add tsqa data loader.

--- EasyLM/data.py
+++ EasyLM/data.py
@@ -718,7 +718,7 @@ class TsqaDataset(JsonTorchDataset):
     def _process_sample(self, sample):
         tokens, labels, attention_mask = self.encode_tsqa_example(
             example=sample,
-            tokenizer=selftokenizer,
+            tokenizer=self.tokenizer,
             max_seq_length=self.config.seq_length,
             target_year=self.config.target_year,
             add_time_in_prompt=self.config.add_time_in_prompt,
