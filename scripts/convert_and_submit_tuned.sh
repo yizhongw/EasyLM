8ae0ba0a9d86faf085f3ee8c4bfe316ac841ef71
Hamish Ivison <hamishivi@gmail.com>
2024-01-26 21:24:05 -0800

    fixes to conversion script

--- scripts/convert_and_submit_tuned.sh
+++ scripts/convert_and_submit_tuned.sh
@@ -7,9 +7,11 @@ MODEL_SIZE=$2
 MODEL_NAME=$3
 WORKSPACE=$4
 
-python -m EasyLM.models.llama.convert_easylm_to_hf --load_checkpoint=params::${MODEL_PATH} --tokenizer_path='gs://hamishi-dev/easylm/llama/tokenizer.model' --model_size=${MODEL_SIZE} --output_dir=tmp
+gsutil cp gs://hamishi-dev/easylm/llama/tokenizer.model tokenizer.model
 
-beaker dataset upload tmp --name ${MODEL_NAME} --workspace ${WORKSPACE} &> tmp.log
+python -m EasyLM.models.llama.convert_easylm_to_hf --load_checkpoint=params::${MODEL_PATH} --tokenizer_path='tokenizer.model' --model_size=${MODEL_SIZE} --output_dir=tmp
+
+beaker dataset create tmp --name ${MODEL_NAME} --workspace ai2/${WORKSPACE} &> tmp.log
 
 # parse beaker id from log. Format: Uploading <name> to <id>
 BEAKER_ID=$(awk '/Uploading/ {print $4}' tmp.log)
