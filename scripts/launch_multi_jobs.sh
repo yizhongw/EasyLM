7367aebeb5eeb7a6c8294b11b1bb2c6430848770
yizhongw <eastonwyz@gmail.com>
2024-02-14 17:43:23 -0800

    updated exps.

--- scripts/launch_multi_jobs.sh
+++ scripts/launch_multi_jobs.sh
@@ -1,33 +1,6 @@
 set -x
 
-MODEL_NAME="llama_70b_implicit_ft_2021_correct_5000" &&
-python3 -m EasyLM.models.llama.llama_train \
-    --mesh_dim='-1,16,16' \
-    --dtype='bf16' \
-    --num_epochs=2 \
-    --log_freq=1 \
-    --save_model_freq=0 \
-    --save_milestone_freq=0 \
-    --load_llama_config='70b' \
-    --update_llama_config='' \
-    --load_dataset_state='' \
-    --load_checkpoint='params::gs://hamishi-east1/easylm/llama2/70b' \
-    --tokenizer.vocab_file='gs://hamishi-east1/easylm/llama/tokenizer.model' \
-    --optimizer.type='adamw' \
-    --optimizer.adamw_optimizer.weight_decay=0.0 \
-    --optimizer.adamw_optimizer.lr=5e-6 \
-    --optimizer.adamw_optimizer.end_lr=0 \
-    --optimizer.adamw_optimizer.warmup_ratio=0.03 \
-    --optimizer.accumulate_gradient_steps=4 \
-    --train_dataset.type='prompt_completion' \
-    --train_dataset.text_processor.fields='[prompt],completion' \
-    --train_dataset.json_torch_dataset.path='../tsqa/reformatted_train_set/llama2_70B/correct/2021.jsonl' \
-    --train_dataset.json_torch_dataset.seq_length=128 \
-    --train_dataset.json_torch_dataset.batch_size=32 \
-    --checkpointer.save_optimizer_state=False \
-    --logger.online=True --logger.project='tsqa' --logger.entity='yizhongw' --logger.prefix=${MODEL_NAME} --logger.prefix_to_id=True \
-    --logger.output_dir="gs://yizhong-east1/tsqa_models/${MODEL_NAME}"
-# MODEL_NAME="llama_70b_implicit_ft_adaptive_5000"
+# MODEL_NAME="llama_70b_implicit_ft_2021_correct_5000" &&
 # python3 -m EasyLM.models.llama.llama_train \
 #     --mesh_dim='-1,16,16' \
 #     --dtype='bf16' \
@@ -48,12 +21,39 @@ python3 -m EasyLM.models.llama.llama_train \
 #     --optimizer.accumulate_gradient_steps=4 \
 #     --train_dataset.type='prompt_completion' \
 #     --train_dataset.text_processor.fields='[prompt],completion' \
-#     --train_dataset.json_torch_dataset.path='../tsqa/reformatted_train_set/llama2_70B/adaptive/adaptive.jsonl' \
+#     --train_dataset.json_torch_dataset.path='../tsqa/reformatted_train_set/llama2_70B/correct/2021.jsonl' \
 #     --train_dataset.json_torch_dataset.seq_length=128 \
 #     --train_dataset.json_torch_dataset.batch_size=32 \
 #     --checkpointer.save_optimizer_state=False \
 #     --logger.online=True --logger.project='tsqa' --logger.entity='yizhongw' --logger.prefix=${MODEL_NAME} --logger.prefix_to_id=True \
-#     --logger.output_dir="gs://yizhong-east1/tsqa_models/${MODEL_NAME}" &&
+#     --logger.output_dir="gs://yizhong-east1/tsqa_models/${MODEL_NAME}"
+MODEL_NAME="llama_70b_implicit_ft_adaptive_5000"
+python3 -m EasyLM.models.llama.llama_train \
+    --mesh_dim='-1,16,16' \
+    --dtype='bf16' \
+    --num_epochs=2 \
+    --log_freq=1 \
+    --save_model_freq=0 \
+    --save_milestone_freq=0 \
+    --load_llama_config='70b' \
+    --update_llama_config='' \
+    --load_dataset_state='' \
+    --load_checkpoint='params::gs://hamishi-east1/easylm/llama2/70b' \
+    --tokenizer.vocab_file='gs://hamishi-east1/easylm/llama/tokenizer.model' \
+    --optimizer.type='adamw' \
+    --optimizer.adamw_optimizer.weight_decay=0.0 \
+    --optimizer.adamw_optimizer.lr=5e-6 \
+    --optimizer.adamw_optimizer.end_lr=0 \
+    --optimizer.adamw_optimizer.warmup_ratio=0.03 \
+    --optimizer.accumulate_gradient_steps=4 \
+    --train_dataset.type='prompt_completion' \
+    --train_dataset.text_processor.fields='[prompt],completion' \
+    --train_dataset.json_torch_dataset.path='../tsqa/reformatted_train_set/llama2_70B/adaptive/adaptive.jsonl' \
+    --train_dataset.json_torch_dataset.seq_length=128 \
+    --train_dataset.json_torch_dataset.batch_size=32 \
+    --checkpointer.save_optimizer_state=False \
+    --logger.online=True --logger.project='tsqa' --logger.entity='yizhongw' --logger.prefix=${MODEL_NAME} --logger.prefix_to_id=True \
+    --logger.output_dir="gs://yizhong-east1/tsqa_models/${MODEL_NAME}" &&
 # MODEL_NAME="llama_70b_implicit_ft_adaptive_all"
 # python3 -m EasyLM.models.llama.llama_train \
 #     --mesh_dim='-1,16,16' \
