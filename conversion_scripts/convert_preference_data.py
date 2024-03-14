a0cc39f075335ab12dcc965388d7f56e3a645a1f
Hamish Ivison <hamishivi@gmail.com>
2024-03-14 16:15:20 -0700

    processing scripts

--- conversion_scripts/convert_preference_data.py
+++ conversion_scripts/convert_preference_data.py
@@ -9,14 +9,39 @@ parser.add_argument('--input_dataset', type=str)
 parser.add_argument('--split', type=str)
 parser.add_argument('--output', type=str)
 parser.add_argument('--seed', type=int, default=42)
-# for now, max 5 million samples.
-parser.add_argument('--max_samples', type=int, default=5_000_000)
+# for now, max 500 thousans samples.
+parser.add_argument('--max_samples', type=int, default=500_000)
 args = parser.parse_args()
 
-dataset = load_dataset(args.input_dataset, split=args.split)
+if '.jsonl' in args.input_dataset:
+    dataset = load_dataset('json', data_files=args.input_dataset, split=args.split)
+else:
+    dataset = load_dataset(args.input_dataset, split=args.split)
 dataset = dataset.shuffle(args.seed).select(range(min(args.max_samples, len(dataset))))
 new_data = []
 random_gen = Random(args.seed)
+
+models = []
+
+ # parse out turns and roles from hh-style turns.
+# used for Nectar and HH-RLHF
+def parse_out_prompt_turns_hh_format(text):
+    prompt_turns = []
+    text_split = text.split('Human:')
+    for entry in text_split:
+        if entry.strip() != '':
+            assistant_split = entry.split('Assistant:')
+            human_text = assistant_split[0].strip()
+            if len(assistant_split) > 1:
+                assistant_text = assistant_split[1].strip()
+                if human_text:
+                    prompt_turns.append({"role": "user", "content": human_text})
+                if assistant_text:
+                    prompt_turns.append({"role": "assistant", "content": assistant_text})
+            else:
+                if human_text:
+                    prompt_turns.append({"role": "user", "content": human_text})
+    return prompt_turns
 
 if args.input_dataset == 'nvidia/HelpSteer':
     # group by prompt
@@ -54,16 +79,15 @@ if args.input_dataset == 'nvidia/HelpSteer':
         })
 elif args.input_dataset == 'berkeley-nest/Nectar':
     for sample in dataset:
-        prompt = sample['prompt'].replace("Human: ", "").replace("Assistant: ", "").strip()
+        # we can have multiturn data
+        initial_turns = parse_out_prompt_turns_hh_format(sample['prompt'])
         answers = sorted(sample['answers'], key=lambda x: x['rank'])
         chosen = answers[0]['answer']
         rejected = random_gen.choice(answers[1:])['answer']
-        chosen =  [
-            {'role': 'user', 'content': prompt},
+        chosen =  initial_turns + [
             {'role': 'assistant', 'content': chosen},
         ]
-        rejected =  [
-            {'role': 'user', 'content': prompt},
+        rejected =  initial_turns + [
             {'role': 'assistant', 'content': rejected},
         ]
         new_data.append({
@@ -78,7 +102,26 @@ elif args.input_dataset == 'argilla/ultrafeedback-binarized-preferences-cleaned'
         new_data.append({
             'chosen': chosen,
             'rejected': rejected,
-            'source': 'argilla-ultrafeedback'
+            'source': 'argilla-ultrafeedback',
+            'margin': sample['chosen-rating'] - sample['rejected-rating']
+        })
+elif args.input_dataset == 'argilla/distilabel-capybara-dpo-7k-binarized':
+    for sample in dataset:
+        chosen = sample['chosen']
+        rejected = sample['rejected']
+        new_data.append({
+            'chosen': chosen,
+            'rejected': rejected,
+            'source': 'argilla-capybara'
+        })
+elif args.input_dataset == 'argilla/dpo-mix-7k':
+    for sample in dataset:
+        chosen = sample['chosen']
+        rejected = sample['rejected']
+        new_data.append({
+            'chosen': chosen,
+            'rejected': rejected,
+            'source': 'argilla-capybara'
         })
 elif args.input_dataset == 'HuggingFaceH4/ultrafeedback_binarized':
     argilla_dataset = load_dataset('argilla/ultrafeedback-binarized-preferences-cleaned', split='train')
@@ -117,28 +160,103 @@ elif args.input_dataset == 'Intel/orca_dpo_pairs':
             'rejected': rejected,
             'source': 'orca_dpo_pairs'
         })
+elif args.input_dataset == 'argilla/distilabel-intel-orca-dpo-pairs':
+    for sample in dataset:
+        prompt = {'role': 'user', 'content': sample['input']}
+        chosen = [prompt, {'role': 'assistant', 'content': sample['chosen']}]
+        rejected = [prompt, {'role': 'assistant', 'content': sample['rejected']}]
+        new_data.append({
+            'chosen': chosen,
+            'rejected': rejected,
+            'source': 'orca_dpo_pairs_argilla'
+        })
+elif args.input_dataset == 'prm800k_train_phase1.jsonl':
+    # Phase 1 of PRM: we sample reasoning paths from the set.
+    # sometimes examples are malformed, so we skip them.
+    for sample in dataset:
+        # only collect samples that reached the solution
+        if sample['label']['finish_reason'] != 'solution':
+            continue
+        prompt = {'role': 'user', 'content': sample['question']['problem']}
+        ground_truth = sample['question']['ground_truth_answer']
+        # start by gathering the full ground truth completion
+        # along the way, we will gather 'wrong' reasoning paths.
+        chosen_completions = []
+        rejected_completions = []
+        error = False
+        # pick one random step to do incorrectly
+        multichoice_steps = [idx for idx, step in enumerate(sample['label']['steps']) if len(step['completions']) > 1 or step['human_completion'] is not None]
+        if len(multichoice_steps) == 0:
+            # malformed, skip
+            continue
+        use_rejected = random_gen.choice(multichoice_steps)
+        for idx, step in enumerate(sample['label']['steps']):
+             # get chosen completion
+            if step['chosen_completion'] is None:
+                if step['human_completion'] is None:
+                    # malformed, skip
+                    error = True
+                    print("Malformed sample, skipping")
+                    break
+                chosen_completion = step['human_completion']['text']
+            else:
+                chosen_completion = step['completions'][step['chosen_completion']]['text']
+            chosen_completions.append(chosen_completion)
+            non_chosen_completions = [x['text'] for x in step['completions'] if x['text'] != chosen_completion]
+            # sometimes there is only one completion for a step. This is fine.
+            if idx == use_rejected:
+                if len(non_chosen_completions) == 0:
+                    # malformed, skip
+                    error = True
+                    print("Malformed sample, skipping")
+                    break
+                rejected_completions.append(random_gen.choice(non_chosen_completions))
+            else:
+                rejected_completions.append(chosen_completion)
+        if error:
+            continue
+        # now, we have the chosen and rejected completions
+        chosen = [prompt, {'role': 'assistant', 'content': '\n'.join(chosen_completions)}]
+        rejected = [prompt, {'role': 'assistant', 'content': '\n'.join(rejected_completions)}]
+        new_data.append({
+            'chosen': chosen,
+            'rejected': rejected,
+            'source': 'prm800k_phase1'
+        })
+elif args.input_dataset == 'prm800k_train_phase2.jsonl':
+    # Phase 2 of PRM: multiple generations for the dataset, which are scored by humans.
+    # first, we group by prompt
+    prompts = {}
+    for sample in dataset:
+        prompt = sample['question']['problem']
+        if prompt not in prompts:
+            prompts[prompt] = []
+        prompts[prompt].append(sample)
+    # now sample our chosen and rejected completions
+    # our chosen is the ground truth, and our rejected is a random sample that answers wrong
+    # we ignore model-generated correct completions for now
+    for prompt, samples in prompts.items():
+        prompt = {'role': 'user', 'content': prompt}
+        ground_truth = samples[0]['question']['ground_truth_solution']
+        chosen = [prompt, {'role': 'assistant', 'content': ground_truth}]
+        # gather model completions that are wrong
+        wrong_completions = [x for x in samples if x['label']['finish_reason'] == 'found_error']
+        if len(wrong_completions) == 0:
+            continue
+        sample = random_gen.choice(wrong_completions)
+        model_generation = '\n'.join(sample['question']['pre_generated_steps'])
+        rejected = [prompt, {'role': 'assistant', 'content': model_generation}]
+        if chosen[1]['content'] is None or rejected[1]['content'] is None:
+            continue
+        new_data.append({
+            'chosen': chosen,
+            'rejected': rejected,
+            'source': 'prm800k_phase2'
+        })
 elif args.input_dataset == "Anthropic/hh-rlhf":
     for sample in dataset:
-        # parse out turns and roles
-        def parse_out_prompt_turns(text):
-            prompt_turns = []
-            text_split = text.split('Human:')
-            for entry in text_split:
-                if entry.strip() != '':
-                    assistant_split = entry.split('Assistant:')
-                    human_text = assistant_split[0].strip()
-                    if len(assistant_split) > 1:
-                        assistant_text = assistant_split[1].strip()
-                        if human_text:
-                            prompt_turns.append({"role": "user", "content": human_text})
-                        if assistant_text:
-                            prompt_turns.append({"role": "assistant", "content": assistant_text})
-                    else:
-                        if human_text:
-                            prompt_turns.append({"role": "user", "content": human_text})
-            return prompt_turns
-        chosen_prompt_turns = parse_out_prompt_turns(sample['chosen'])
-        rejected_prompt_turns = parse_out_prompt_turns(sample['rejected'])
+        chosen_prompt_turns = parse_out_prompt_turns_hh_format(sample['chosen'])
+        rejected_prompt_turns = parse_out_prompt_turns_hh_format(sample['rejected'])
         # run through the turns until they mismatch. This is our comparison point
         # sometimes the conversation keeps going on one, but just ignore that
         prompt_turns = []
@@ -155,8 +273,8 @@ elif args.input_dataset == "Anthropic/hh-rlhf":
         final_chosen_turn = chosen_prompt_turns[len(prompt_turns)]
         final_rejected_turn = rejected_prompt_turns[len(prompt_turns)]
         new_data.append({
-            'chosen': prompt_turns + chosen_prompt_turns,
-            'rejected': prompt_turns + rejected_prompt_turns,
+            'chosen': prompt_turns + [final_chosen_turn],
+            'rejected': prompt_turns + [final_rejected_turn],
             'source': 'hh-rlhf'
         })
 elif args.input_dataset == "lvwerra/stack-exchange-paired":
--- conversion_scripts/create_preference_data.sh
+++ conversion_scripts/create_preference_data.sh
@@ -12,9 +12,19 @@ python conversion_scripts/convert_preference_data.py --input_dataset stanfordnlp
 
 python conversion_scripts/convert_preference_data.py --input_dataset Intel/orca_dpo_pairs --split train --output converted_pref_data/orca_dpo_pairs.jsonl
 
+python conversion_scripts/convert_preference_data.py --input_dataset argilla/distilabel-intel-orca-dpo-pairs --split train --output converted_pref_data/argilla_orca_dpo_pairs.jsonl
+
 python conversion_scripts/convert_preference_data.py --input_dataset lvwerra/stack-exchange-paired --split train --output converted_pref_data/stack_exchange_paired.jsonl
 
 python conversion_scripts/convert_preference_data.py --input_dataset Anthropic/hh-rlhf --split train --output converted_pref_data/anthropic_hh.jsonl
+
+# prm800k isnt on hf
+wget https://github.com/openai/prm800k/raw/main/prm800k/data/phase1_train.jsonl -O prm800k_train_phase1.jsonl
+wget https://github.com/openai/prm800k/raw/main/prm800k/data/phase2_train.jsonl -O prm800k_train_phase2.jsonl
+
+python conversion_scripts/convert_preference_data.py --input_dataset prm800k_train_phase1.jsonl --split train --output converted_pref_data/prm800k_pairs_phase1.jsonl
+python conversion_scripts/convert_preference_data.py --input_dataset prm800k_train_phase2.jsonl --split train --output converted_pref_data/prm800k_pairs_phase2.jsonl
+cat converted_pref_data/prm800k_pairs_phase1.jsonl converted_pref_data/prm800k_pairs_phase2.jsonl > converted_pref_data/prm800k_pairs.jsonl
 
 # combine and upload to hf, where each dataset is a diff split.
 # python conversion_scripts/combine_preference_data.py --input_dir converted_pref_data --output_dir combined_pref_data
